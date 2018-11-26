42d2944802ba254843cad0736504382e1ddac37a
Lydia Simmons <saybaar@gmail.com>
2018-11-25 21:17:58 -0800

    added multi-user support via sockets and automatic cleanup

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var express = require('express');
-var socket = require('socket.io');
+
 var parser = require('body-parser');
 var process = require('process');
-var fs = require('fs');
-const { spawn } = require('child_process');
+var socket = require('socket.io');
+var fs = require('fs-extra');
+const {
+  spawn
+} = require('child_process');
 /*
 ls.stdout.on('data', (data) => {
   console.log('' + data);
@@ -20,66 +23,87 @@ ls.stdout.on('data', (data) => {
   // (lualatex does its own file generation)
 });
 */
-var guid = "1234567890";
 
 var server = express();
+var io = socket(server.listen(process.env.PORT || 8080));
 server.use(parser.urlencoded());
 
 server.get('/', function(req, res) {
-  console.log('starting in directory ' + process.cwd());
-  // make a temp directory; when scalable, this will happen inside a socket
-  // connection event instead
-  fs.mkdir(__dirname + '/' + guid, (err) => {
-    if (err.code != 'EEXIST') { throw err; }
-    process.chdir('./' + guid);
-    console.log('now in directory ' + process.cwd());
-    res.status(200);
-    res.type('html');
-    res.sendFile(__dirname + '/index.html');
-  });
+  res.status(200);
+  res.type('html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 server.get('/example.gabc', function(req, res) {
   res.status(200);
   res.type('text/plain');
   res.sendFile(__dirname + '/example.gabc');
-})
-
-// Receive a POST with gabc body
-server.post('/generate', function(req, res) {
-  var gabc = req.body.gabc;
-  var gabc_file = 'example.gabc';
-  fs.writeFile(gabc_file, gabc, (err) => {
-    if (err) throw err;
-    console.log('saved some GABC');
-  });
-  const render = spawn('lualatex', ['--shell-escape', 'test.tex']);
-  var output = '';
-
-  render.stderr.on('data', (data) => {
-    console.log(data);
-    output = output + 'stderr says: ' + data + '\n';
-  });
-
-  render.on('close', (code) => {
-    console.log('render process exited with code ' + code);
-    if (code === 0) {
-      res.status(200);
-      res.type('text/plain');
-      res.send();
-    } else {
-      res.status(400);
-      res.type('text/plain');
-      res.send(output);
-    }
-  });
 });
 
 server.get('/:guid/test.pdf', function(req, res) {
   res.status(200);
   res.type('application/pdf');
-  res.sendFile(__dirname + '/' + guid + '/test.pdf');
-})
+  res.sendFile(__dirname + '/' + req.params.guid + '/test.pdf');
+});
+
+io.on('connection', function(objectSocket) {
+  console.log('starting in directory ' + process.cwd());
+  // make a temp directory; when scalable, this will happen inside a socket
+  // connection event instead
+  var newdir = __dirname + '/' + objectSocket.id;
+  fs.mkdir(newdir, (err) => {
+    if (err && err.code != 'EEXIST') {
+      throw err;
+    }
+    fs.copyFileSync(__dirname + '/test.tex', newdir + '/test.tex');
+  });
+
+  objectSocket.on('message', function(objectData) {
+    renderPdf(objectData.gabc);
+  });
+
+  objectSocket.on('disconnect', function() {
+    fs.remove(objectSocket.id, function() {
+      console.log('removed directory ' + objectSocket.id);
+    })
+  });
+
+  var renderPdf = function(gabc) {
+    process.chdir(__dirname + '/' + objectSocket.id); // todo: error check
+    var gabc_file = 'example.gabc';
+    fs.writeFile(gabc_file, gabc, (err) => {
+      if (err) throw err;
+      console.log('saved some GABC');
+    });
+
+    const render = spawn('lualatex',
+      ['--shell-escape', '-interaction=nonstopmode', 'test.tex']);
+    var output = '';
+
+    render.stderr.on('data', (data) => {
+      console.log(data);
+      output = output + 'error: ' + data + '\n';
+    });
+
+    render.on('close', (code) => {
+      console.log('render process exited with code ' + code);
+      if (code === 0) {
+        objectSocket.emit('message', {
+          'status': 'done'
+        });
+      } else {
+        objectSocket.emit('message', {
+          'status': 'error',
+          'log': output
+        });
+      }
+    });
+    console.log('finished, moving out of ' + process.cwd());
+    process.chdir(__dirname);
+  };
+
+});
+
 /*
 server.get('/test.mid', function(req, res) {
   res.status(200);
@@ -87,4 +111,3 @@ server.get('/test.mid', function(req, res) {
   res.sendFile('/home/lydia/webdev-project/test.mid');
 })
 */
-server.listen(8080);
