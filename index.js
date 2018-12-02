bab9cda1e0ea634c9800a401cf26da390ab4107f
Lydia Simmons <saybaar@gmail.com>
2018-12-01 16:33:30 -0800

    add audio rendering and cache control

--- index.js
+++ index.js
@@ -12,51 +12,44 @@ const {
 var server = express();
 var io = socket(server.listen(8080));
 server.use(parser.urlencoded());
-
-server.get('/', function(req, res) {
-  res.status(200);
-  res.type('html');
-  res.sendFile(__dirname + '/index.html');
-});
-
-server.get('/example.gabc', function(req, res) {
-  res.status(200);
-  res.type('text/plain');
-  res.sendFile(__dirname + '/example.gabc');
-});
-
-server.get('/short_example.gabc', function(req, res) {
-  res.status(200);
-  res.type('text/plain');
-  res.sendFile(__dirname + '/short_example.gabc');
-});
+express.static.mime.define({'text/plain': ['gabc']});
+server.use(express.static('static'));
 
 server.get('/:guid/test.pdf', function(req, res) {
   res.status(200);
   res.type('application/pdf');
+  res.set('Cache-Control', 'private, max-age=0, no-cache, no-store');
   res.sendFile(__dirname + '/' + req.params.guid + '/test.pdf');
 });
 
 server.get('/:guid/test.png', function(req, res) {
   res.status(200);
   res.type('image/png');
+  res.set('Cache-Control', 'private, max-age=0, no-cache, no-store');
   res.sendFile(__dirname + '/' + req.params.guid + '/test.png');
 });
 
+server.get('/:guid/test.wav', function(req, res) {
+  res.status(200);
+  res.type('audio/wav');
+  res.set('Cache-Control', 'private, max-age=0, no-cache, no-store');
+  res.sendFile(__dirname + '/' + req.params.guid + '/test.wav');
+})
+
 io.on('connection', function(objectSocket) {
   console.log('starting in directory ' + process.cwd());
-  // make a temp directory; when scalable, this will happen inside a socket
-  // connection event instead
   var newdir = __dirname + '/' + objectSocket.id;
   fs.mkdir(newdir, (err) => {
     if (err && err.code != 'EEXIST') {
       throw err;
     }
-    fs.copyFileSync(__dirname + '/test.tex', newdir + '/test.tex');
+    fs.copyFileSync(__dirname + '/static/test.tex', newdir + '/test.tex');
   });
 
-  objectSocket.on('message', function(objectData) {
-    renderPdf(objectData.gabc);
+  objectSocket.on('gen', function(objectData) {
+    copyGABC(objectData.gabc);
+    renderPdf();
+    renderAudio();
   });
 
   objectSocket.on('disconnect', function() {
@@ -65,14 +58,17 @@ io.on('connection', function(objectSocket) {
     })
   });
 
-  var renderPdf = function(gabc) {
+  var copyGABC = function(gabc) {
     process.chdir(__dirname + '/' + objectSocket.id); // todo: error check
     var gabc_file = 'example.gabc';
     fs.writeFile(gabc_file, gabc, (err) => {
       if (err) throw err;
       console.log('saved some GABC');
     });
+  };
 
+  var renderPdf = function() {
+    process.chdir(__dirname + '/' + objectSocket.id); // todo: error check
     const render = spawn('lualatex',
       ['--shell-escape', '-interaction=nonstopmode', 'test.tex']);
     var output = '';
@@ -83,11 +79,11 @@ io.on('connection', function(objectSocket) {
     });
 
     render.on('close', (code) => {
-      console.log('render process exited with code ' + code);
+      console.log('lualatex exited with code ' + code);
       if (code === 0) {
         renderImage();
       } else {
-        objectSocket.emit('message', {
+        objectSocket.emit('error', {
           'status': 'error',
           'log': output
         });
@@ -111,11 +107,11 @@ io.on('connection', function(objectSocket) {
     image_render.on('close', (code) => {
       console.log('image render process exited with code ' + code);
       if (code === 0) {
-        objectSocket.emit('message', {
+        objectSocket.emit('image', {
           'status': 'done'
         });
       } else {
-        objectSocket.emit('message', {
+        objectSocket.emit('error', {
           'status': 'error',
           'log': output
         });
@@ -125,12 +121,56 @@ io.on('connection', function(objectSocket) {
     process.chdir(__dirname);
   };
 
-});
+  var renderAudio = function() {
+    process.chdir(__dirname + '/' + objectSocket.id); // todo: error check
+    var gabc_file = 'example.gabc';
+    const midi_convert = spawn(__dirname + '/gabctk/gabctk.py', ['-o', 'test.mid', gabc_file]);
+    var output = '';
 
-/*
-server.get('/test.mid', function(req, res) {
-  res.status(200);
-  res.type('application/midi');
-  res.sendFile('/home/lydia/webdev-project/test.mid');
-})
-*/
+    midi_convert.stderr.on('data', (data) => {
+      console.log(data);
+      output = output + 'error: ' + data + '\n';
+    });
+
+    midi_convert.on('close', (code) => {
+      console.log('gabctk process exited with code ' + code);
+      if (code === 0) {
+        convertAudio();
+      } else {
+        objectSocket.emit('error', {
+          'status': 'error',
+          'log': output
+        });
+      }
+    });
+    process.chdir(__dirname);
+  };
+
+  var convertAudio = function() {
+    process.chdir(__dirname + '/' + objectSocket.id); // todo: error check
+    var midi_file = 'test.mid';
+    const wav_convert = spawn('timidity', ['-Ow', '-o', 'test.wav', midi_file]);
+    var output = '';
+
+    wav_convert.stderr.on('data', (data) => {
+      console.log(data);
+      output = output + 'error: ' + data + '\n';
+    });
+
+    wav_convert.on('close', (code) => {
+      console.log('timidity process exited with code ' + code);
+      if (code === 0) {
+        objectSocket.emit('audio', {
+          'status':'done'
+        });
+      } else {
+        objectSocket.emit('error', {
+          'status': 'error',
+          'log': output
+        });
+      }
+    });
+    process.chdir(__dirname);
+  };
+
+});
