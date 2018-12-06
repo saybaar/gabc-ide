53dca15c380e3a303b1cb0fdff06a784529b33a8
Lydia Simmons <saybaar@gmail.com>
2018-12-06 14:22:57 -0800

    Renamed generated files from "test" to "gen"

--- index.js
+++ index.js
@@ -13,29 +13,29 @@ var server = express();
 var io = socket(server.listen(8080));
 server.use(parser.urlencoded());
 express.static.mime.define({
-  'text/plain': ['gabc']
+  'text/plain': ['gabc','tex']
 });
 server.use(express.static('static'));
 
-server.get('/:guid/test.pdf', function(req, res) {
+server.get('/:guid/gen.pdf', function(req, res) {
   res.status(200);
   res.type('application/pdf');
   res.set('Cache-Control', 'private, max-age=0, no-cache, no-store');
-  res.sendFile(__dirname + '/temp/' + req.params.guid + '/test.pdf');
+  res.sendFile(__dirname + '/temp/' + req.params.guid + '/gen.pdf');
 });
 
-server.get('/:guid/test.png', function(req, res) {
+server.get('/:guid/gen.png', function(req, res) {
   res.status(200);
   res.type('image/png');
   res.set('Cache-Control', 'private, max-age=0, no-cache, no-store');
-  res.sendFile(__dirname + '/temp/' + req.params.guid + '/test.png');
+  res.sendFile(__dirname + '/temp/' + req.params.guid + '/gen.png');
 });
 
-server.get('/:guid/test.wav', function(req, res) {
+server.get('/:guid/gen.wav', function(req, res) {
   res.status(200);
   res.type('audio/wav');
   res.set('Cache-Control', 'private, max-age=0, no-cache, no-store');
-  res.sendFile(__dirname + '/temp/' + req.params.guid + '/test.wav');
+  res.sendFile(__dirname + '/temp/' + req.params.guid + '/gen.wav');
 });
 
 io.on('connection', function(objectSocket) {
@@ -44,7 +44,7 @@ io.on('connection', function(objectSocket) {
     if (err && err.code != 'EEXIST') {
       throw err;
     }
-    fs.copyFileSync(__dirname + '/static/test.tex', newdir + '/test.tex');
+    fs.copyFileSync(__dirname + '/static/gen.tex', newdir + '/gen.tex');
   });
 
   objectSocket.on('disconnect', function() {
@@ -68,7 +68,7 @@ io.on('connection', function(objectSocket) {
 
   var copyGABC = function(gabc) {
     process.chdir(__dirname + '/temp/' + objectSocket.id); // todo: error check
-    var gabc_file = 'example.gabc';
+    var gabc_file = 'input.gabc';
     try {
       fs.writeFileSync(gabc_file, gabc);
     } catch (error) {
@@ -81,7 +81,7 @@ io.on('connection', function(objectSocket) {
   var renderPdf = function() {
     process.chdir(__dirname + '/temp/' + objectSocket.id); // todo: error check
     const render = spawn('lualatex',
-      ['--shell-escape', '-interaction=nonstopmode', 'test.tex']);
+      ['--shell-escape', '-interaction=nonstopmode', 'gen.tex']);
     var output = '';
 
     render.stderr.on('data', (data) => {
@@ -101,8 +101,8 @@ io.on('connection', function(objectSocket) {
 
   var renderImage = function() {
     process.chdir(__dirname + '/temp/' + objectSocket.id); // todo: error check
-    var pdf_file = 'test.pdf';
-    const image_render = spawn('convert', ['-density', '300', pdf_file, '-quality', '90', 'test.png']);
+    var pdf_file = 'gen.pdf';
+    const image_render = spawn('convert', ['-density', '300', pdf_file, '-quality', '90', 'gen.png']);
     var output = '';
 
     image_render.stderr.on('data', (data) => {
@@ -124,8 +124,8 @@ io.on('connection', function(objectSocket) {
 
   var renderAudio = function() {
     process.chdir(__dirname + '/temp/' + objectSocket.id); // todo: error check
-    var gabc_file = 'example.gabc';
-    const midi_convert = spawn(__dirname + '/gabctk/gabctk.py', ['-o', 'test.mid', gabc_file]);
+    var gabc_file = 'input.gabc';
+    const midi_convert = spawn(__dirname + '/gabctk/gabctk.py', ['-o', 'gen.mid', gabc_file]);
     var output = '';
 
     midi_convert.stderr.on('data', (data) => {
@@ -145,8 +145,8 @@ io.on('connection', function(objectSocket) {
 
   var convertAudio = function() {
     process.chdir(__dirname + '/temp/' + objectSocket.id); // todo: error check
-    var midi_file = 'test.mid';
-    const wav_convert = spawn('timidity', ['-Ow', '-o', 'test.wav', midi_file]);
+    var midi_file = 'gen.mid';
+    const wav_convert = spawn('timidity', ['-Ow', '-o', 'gen.wav', midi_file]);
     var output = '';
 
     wav_convert.stderr.on('data', (data) => {
